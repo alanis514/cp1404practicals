b5fbfc989407084c113c6e882336f95ab72b63f8
alanis514 <alanis_514@yahoo.com>
2020-09-01 18:39:39 +1000

    Initial commit of lists_warmup.py

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,27 @@
+"'CP1404 prac_04"
+"list_warmup.py"
+
+numbers = [3, 1, 4, 1, 5, 9, 2]
+
+numbers[0]
+numbers[-1]
+numbers[3]
+numbers[:-1]
+numbers[3:4]
+5 in numbers
+7 in numbers
+"3" in numbers
+numbers + [6, 5, 3]
+
+
+# 1
+numbers[0] = "ten"
+
+# 2
+numbers[-1] = 1
+
+# 3
+numbers[2:]
+
+# 4
+9 in numbers
